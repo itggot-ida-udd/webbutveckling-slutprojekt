99d3f25a29701a69962d4113996d4b4d4329c3d2
itggot-ida-udd <ida.udd@elev.ga.ntig.se>
2019-05-06 15:00:07 +0200

    fixes funktioner. och skapandet av nya delar och element till sidan

--- layout/site/js/main.js
+++ layout/site/js/main.js
@@ -1,4 +1,34 @@
 
 $(".button").click(function(){
-    $(this).parents().find(".cover").hide(200);
+    $(this).parents().find(".cover").slideUp(200);
   });
+
+$(".menu_background").hide();
+$(".menu").hide();
+
+$("#close").click(function(){
+    $(this).parents().find(".menu").hide(200);
+    $(this).parents().find(".menu_background").hide();
+  });
+
+$(".menu_background").click(function(){
+    $(this).parents().find(".menu").hide(200);
+    $(this).hide();
+  });
+
+$("#hamburger-menu").click(function(){
+   $(this).parents().find(".menu").show(200)
+   $(this).parents().find(".menu_background").show()
+  });  
+
+
+  $(".hover_con").hide();
+
+  $("#info").hover(function(){
+    $(this).find(".hover_con").show();
+  },function(){
+    $(this).find(".hover_con").hide();
+
+  });
+
+
